3064ae0409362c449d85c89a1d4b0c1b4cb60bbd
BloodyRoom <sabadashkamaksym@gmail.com>
2026-02-17 18:33:19 +0200

    add identity

---
+++ RestaurantAPI/Domain/Entities/Identity/UserEntity.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Domain.Entities.Identity;
+
+public class UserEntity : IdentityUser<long>
+{
+    public DateTime DateCreated { get; set; } = DateTime.SpecifyKind(DateTime.Now, DateTimeKind.Utc);
+    public string? FirstName { get; set; } = null;
+    public string? LastName { get; set; } = null;
+
+    public virtual ICollection<UserRoleEntity>? UserRoles { get; set; }
+    public virtual ICollection<UserLoginEntity>? UserLogins { get; set; }
+}
