67b71f4e6cc755e38d03183e818139b3ca10b0a1
BloodyRoom <sabadashkamaksym@gmail.com>
2026-02-24 00:04:56 +0200

    some fixes

--- RestaurantAPI/Domain/Entities/CategoryEntity.cs
+++ RestaurantAPI/Domain/Entities/CategoryEntity.cs
@@ -1,11 +1,12 @@
 ﻿using Domain.Entities.Base;
+using Domain.Interfaces;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Domain.Entities;
 
 [Table("tblCategories")]
-public class CategoryEntity : BaseEntity<long>
+public class CategoryEntity : BaseEntity<long>, IHasImage
 {
     [StringLength(100)]
     public string Name { get; set; } = string.Empty;
