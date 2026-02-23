a4d1a4c55970a272d2ab9669e7f454c9251cc6e1
BloodyRoom <sabadashkamaksym@gmail.com>
2026-02-23 23:08:37 +0200

    transfered register to mediatr

---
+++ RestaurantAPI/Core/Handlers/Account/RegisterCommandHandler.cs
@@ -0,0 +1,64 @@
+﻿using AutoMapper;
+using Core.Commands.Account;
+using Core.Constants;
+using Core.Interfaces;
+using Core.Models;
+using Core.Models.Account;
+using Domain.Entities.Identity;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Handlers.Account;
+
+public class RegisterCommandHandler
+    : IRequestHandler<RegisterCommand, Result<AuthResponse>>
+{
+    private readonly UserManager<UserEntity> _userManager;
+    private readonly SignInManager<UserEntity> _signInManager;
+    private readonly IJwtTokenService _jwtTokenService;
+    private readonly IMapper _mapper;
+    private readonly RoleManager<RoleEntity> _roleManager;
+
+    public RegisterCommandHandler(
+        UserManager<UserEntity> userManager,
+        SignInManager<UserEntity> signInManager,
+        IJwtTokenService jwtTokenService,
+        IMapper mapper,
+        RoleManager<RoleEntity> roleManager)
+    {
+        _userManager = userManager;
+        _signInManager = signInManager;
+        _jwtTokenService = jwtTokenService;
+        _mapper = mapper;
+        _roleManager = roleManager;
+    }
+
+    public async Task<Result<AuthResponse>> Handle(RegisterCommand request, CancellationToken cancellationToken)
+    {
+        var user = _mapper.Map<UserEntity>(request.model);
+
+        var result = await _userManager.CreateAsync(user, request.model.Password);
+
+        if (!result.Succeeded)
+        {
+            var errors = string.Join("\n",
+                result.Errors.Select(e => e.Description));
+            
+            return Result<AuthResponse>.Failure(errors);
+        }
+
+        if (!await _roleManager.RoleExistsAsync(Roles.User))
+        {
+            await _roleManager.CreateAsync(
+                new RoleEntity { Name = Roles.User });
+        }
+
+        await _userManager.AddToRoleAsync(user, Roles.User);
+
+        var token = await _jwtTokenService.CreateTokenAsync(user);
+        return Result<AuthResponse>.Success(new AuthResponse(token));
+    }
+}
--- RestaurantAPI/RestaurantAPI/Controllers/AccountController.cs
+++ RestaurantAPI/RestaurantAPI/Controllers/AccountController.cs
@@ -34,52 +34,12 @@ public class AccountController(UserManager<UserEntity> userManager,
 
 
     [HttpPost]
-    public async Task<IActionResult> Register([FromForm] RegisterModel model)
+    public async Task<IActionResult> Register([FromBody] RegisterModel model)
     {
-        var user = mapper.Map<UserEntity>(model);
+        var result = await mediator.Send(new RegisterCommand(model));
 
-        var result = await userManager.CreateAsync(user, model.Password);
-        if (result.Succeeded)
-        {
-            try
-            {
-                result = await userManager.AddToRoleAsync(user, Roles.User);
-                if (result.Succeeded)
-                {
-                    var token = await jwtTokenService.CreateTokenAsync(user);
-                    return Ok(new
-                    {
-                        token
-                    });
-                }
-            }
-            catch (InvalidOperationException) // коли немає створеної ролі в бд, то створюємо її
-            {
-                result = await roleManager.CreateAsync(new RoleEntity { Name = Roles.User });
-                if (result.Succeeded)
-                {
-                    result = await userManager.AddToRoleAsync(user, Roles.User);
-                    if (result.Succeeded)
-                    {
-                        var token = await jwtTokenService.CreateTokenAsync(user);
-                        return Ok(new
-                        {
-                            token
-                        });
-                    }
-                }
-            }
-        }
-        else
-        {
-            string errorMsg = "";
-            foreach (var error in result.Errors)
-            {
-                errorMsg += $"{error.Description}\n";
-            }
-            return BadRequest(errorMsg);
-        }
+        if (!result.IsSuccess) return BadRequest(result.Error);
 
-        return BadRequest("Виникла помилка під час реєстрації, повторіть спробу пізніше.");
+        return Ok(result.Value);
     }
 }
