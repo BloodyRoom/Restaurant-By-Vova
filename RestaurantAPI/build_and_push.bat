1c6cb76529f4324973379285ddf3f0df65b6f3dd
novakvova <novakvova@gmail.com>
2026-03-08 16:05:10 +0200

    Upload Backend

--- RestaurantAPI/build_and_push.bat
+++ RestaurantAPI/build_and_push.bat
@@ -4,13 +4,13 @@ echo Docker login...
 docker login
 
 echo Building Docker image api...
-docker build -t restaurant-api . 
+docker build -t api-u-vovy . 
 
 echo Tagging Docker image api...
-docker tag restaurant-api:latest novakvova/restaurant-api:latest
+docker tag api-u-vovy:latest novakvova/api-u-vovy:latest
 
 echo Pushing Docker image api to repository...
-docker push novakvova/restaurant-api:latest
+docker push novakvova/api-u-vovy:latest
 
 echo Done ---api---!
 pause
